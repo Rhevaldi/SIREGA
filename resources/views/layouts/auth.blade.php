dc1ca9876113b3500ae7483950f508ca48b19ada
rhevaldi <rhevaldy8889@gmail.com>
2025-12-22 22:10:20 +0800

    Feat: login ui

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Login | SIREGA')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- AdminLTE -->
+    <link rel="stylesheet" href="{{ asset('adminlte/css/adminlte.min.css') }}">
+</head>
+
+<body class="hold-transition login-page">
+
+    @yield('content')
+
+    <!-- jQuery -->
+    <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE -->
+    <script src="{{ asset('adminlte/js/adminlte.min.js') }}"></script>
+</body>
+</html>
