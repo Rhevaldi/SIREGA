62d28c1609b75687df8634c428e5d41b2b086bdb
Zein Aulia Savara <zein.zeot@gmail.com>
2025-12-22 21:12:25 +0800

    fix: Controller BansosPenerima

--- app/Http/Controllers/BansosPenerimaController.php
+++ app/Http/Controllers/BansosPenerimaController.php
@@ -2,31 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Warga;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BansosPenerimaController extends Controller
 {
-   public function store(Request $request)
-{
-    $request->validate([
-        'warga_id' => 'required',
-        'bansos_id' => 'required',
-        'tanggal_penerimaan' => 'required|date',
-        'status' => 'required'
-    ]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'warga_id' => 'required',
+            'bansos_id' => 'required',
+            'tanggal_penerimaan' => 'required|date',
+            'status' => 'required'
+        ]);
 
-    DB::table('bansos_penerima')->insert([
-        'warga_id' => $request->warga_id,
-        'bansos_id' => $request->bansos_id,
-        'tanggal_penerimaan' => $request->tanggal_penerimaan,
-        'status' => $request->status,
-        'keterangan' => $request->keterangan,
-        'created_at' => now(),
-        'updated_at' => now()
-    ]);
+        DB::table('bansos_penerima')->insert([
+            'warga_id' => $request->warga_id,
+            'bansos_id' => $request->bansos_id,
+            'tanggal_penerimaan' => $request->tanggal_penerimaan,
+            'status' => $request->status,
+            'keterangan' => $request->keterangan,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
 
-    return back()->with('success', 'Data penerima bansos berhasil disimpan');
-}
-
+        return back()->with('success', 'Data penerima bansos berhasil disimpan');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Bansos;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             DesaSeeder::class,
             KategoriSeeder::class,
+            BansosSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
